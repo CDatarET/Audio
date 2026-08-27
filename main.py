697584e4e0246bd9ae64180f86e1e32c7431083f
CdatarET <datar.chaitanya@gmail.com>
2026-08-27 23:51:46 +0530

    create fixed

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import subprocess as sp
+import shlex
+import os
 
 def command():
     print("Enter command or type help: ", end = "")
     line = input()
-    return line.split()
+    return shlex.split(line)
 
 #main
 print("Welcome to Terminal Music Player!")
 
 cmd = command()
+print(cmd)
+
 if len(cmd) == 0 or cmd[0] == "help":
     print("Available commands-")
     print("create <playlist>")
@@ -21,18 +25,23 @@ elif cmd[0] == "create":
     if len(cmd) < 2:
         print("Invalid arguments!")
     else:
-        sp.run(['touch', 'Playlists/' + " ".join(cmd[1:])])
+        for i in range(1, len(cmd)):
+            sp.run(['touch', 'Playlists/' + cmd[i]])
 
 elif(cmd[0] == "add"):
     if len(cmd) < 3:
         print("Invalid arguments!")
     else:
-        sp.run(['mv', cmd[1], 'Music/' + cmd[1]])
+        cmd[1] = os.path.expanduser(cmd[1])
+        fname = cmd[1][cmd[1].rfind('/') + 1:]
+        sp.run(['mv', cmd[1], './Music/' + fname])
+        '''
         path = cmd[1].split('/')
         fname = 'Music/' + path[len(path) - 1]
         for i in range(2, len(cmd)):
             with open(cmd[i], "a") as file:
                 file.write('Music/' + fname)
+        '''
 
 elif(args[0] == "play"):
     print("placeholder")
